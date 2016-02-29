17cc9b272e117750d57c1496cd1a1efb03856dd5
Paweł Jasiaczyk <pawel.jasiaczyk@gmail.com>
2016-02-29 10:15:04 +0100

    First Commit

---
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Models;
+using SportsStore.WebUI.HtmlHelpers;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Can_Paginate()
+        {
+            // przygotowanie
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product{ProductID = 1, Name = "P1"},
+                new Product{ProductID = 2, Name = "P2"},
+                new Product{ProductID = 3, Name = "P3"},
+                new Product{ProductID = 4, Name = "P4"},
+                new Product{ProductID = 5, Name = "P5"},
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // działanie
+            IEnumerable<Product> result =
+                (IEnumerable<Product>)controller.List(2).Model;
+
+            // assercje
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            // przygotowanie - definiowanie metody pomocniczej html. Potrzebujemy tego,
+            // aby użyć metody rozszerzającej
+            HtmlHelper myHelper = null;
+
+            // przygotowanie - tworzenie danych PagingInfo
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            // przygotowanie - konfiguracja delegata z użyciem wyrażenia lambda
+            Func<int, string> pageUrlDelegate = i => "Strona" + i;
+
+            // działanie
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            // assercje
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Strona1"">1</a><a class=""btn btn-default btn-primary selected"" href=""Strona2"">2</a><a class=""btn btn-default"" href=""Strona3"">3</a>", result.ToString());
+        }
+
+        // powyższe - crash na poziomie maszyny. Bez netu nie ogarnę. Wrócić tutaj! Trzeba ogarnąć!!!
+        [TestMethod]
+        public void Can_Send_Pagination_View_Model()
+        {
+            // przygotowanie
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]{
+                new Product{ProductID = 1, Name = "P1"},
+                new Product{ProductID = 2, Name = "P2"},
+                new Product{ProductID = 3, Name = "P3"},
+                new Product{ProductID = 4, Name = "P4"},
+                new Product{ProductID = 5, Name = "P5"},
+            });
+
+            // przygotowanie
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // działanie
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+
+            // assercje
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
+        }
+
+    }
+}
---
+++ SportsStore.WebUI/Infastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using Moq;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
+using SportsStore.Domain.Abstract;
+
+
+namespace SportsStore.WebUI.Infastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            this.kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+    }
+}
