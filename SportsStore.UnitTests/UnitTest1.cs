45e64e6eb39207de73d71916bf3468cdffdaa77c
Paweł Jasiaczyk <pawel.jasiaczyk@gmail.com>
2016-02-29 12:02:32 +0100

    Str 206 ukończona

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -32,7 +32,7 @@ namespace SportsStore.UnitTests
             controller.PageSize = 3;
 
             // działanie
-            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(null, 2).Model;
 
             // assercje
             Product[] prodArray = result.Products.ToArray();
@@ -85,7 +85,7 @@ namespace SportsStore.UnitTests
             controller.PageSize = 3;
 
             // działanie
-            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(null, 2).Model;
 
             // assercje
             PagingInfo pageInfo = result.PagingInfo;
@@ -95,5 +95,32 @@ namespace SportsStore.UnitTests
             Assert.AreEqual(pageInfo.TotalPages, 2);
         }
 
+        [TestMethod]
+        public void Can_Filter_Products()
+        {
+            // przygotowanie
+            // -- utworzenie imitacji repozytorium
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]{
+                new Product{ProductID = 1, Name = "P1", Category = "Cat1"},
+                new Product{ProductID = 2, Name = "P2", Category = "Cat2"},
+                new Product{ProductID = 3, Name = "P3", Category = "Cat1"},
+                new Product{ProductID = 4, Name = "P4", Category = "Cat2"},
+                new Product{ProductID = 5, Name = "P5", Category = "Cat3"}
+            });
+
+            // przygotowanie - utworzenie kontrolera i ustawienie 3-elementowej strony
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // działanie
+            Product[] result = ((ProductsListViewModel)controller.List("Cat2", 1).Model)
+                .Products.ToArray();
+
+            // asercje
+            Assert.AreEqual(result.Length, 2);
+            Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
+        }
     }
 }
